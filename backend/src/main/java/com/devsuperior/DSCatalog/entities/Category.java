9b4b225007f56b2405fae01ed727a3c2a445371e
Mateus Viana <mateusvvf@gmail.com>
2025-11-19 11:30:34 -0300

    Product entity, SQL script

--- backend/src/main/java/com/devsuperior/DSCatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/DSCatalog/entities/Category.java
@@ -1,13 +1,16 @@
 package com.devsuperior.DSCatalog.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -28,6 +31,8 @@ public class Category {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();	
 	
 	public Category() {
 		
