078f3e753d9c73ec29da603e55bd270d4551fa1e
Mateus Viana <mateusvvf@gmail.com>
2025-12-08 13:31:04 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/DSCatalog/services/UserService.java
@@ -0,0 +1,99 @@
+package com.devsuperior.DSCatalog.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.DSCatalog.dto.RoleDTO;
+import com.devsuperior.DSCatalog.dto.UserDTO;
+import com.devsuperior.DSCatalog.dto.UserInsertDTO;
+import com.devsuperior.DSCatalog.entities.Role;
+import com.devsuperior.DSCatalog.entities.User;
+import com.devsuperior.DSCatalog.repositories.RoleRepository;
+import com.devsuperior.DSCatalog.repositories.UserRepository;
+import com.devsuperior.DSCatalog.services.exceptions.DatabaseException;
+import com.devsuperior.DSCatalog.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAll(Pageable pageable) {
+		Page<User> users = repository.findAll(pageable);
+		return users.map(obj -> new UserDTO(obj));
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> result = repository.findById(id);
+		User user = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
+		return new UserDTO(user);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+			User entity = new User();
+			copyDtoToEntity(dto, entity);
+			entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID not found: " + id);
+		}
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("ID not found: " + id);
+		}
+		try {
+			repository.deleteById(id);
+			repository.flush();
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Referential integrity violation");
+		}
+	}
+	
+	
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		
+		entity.getRoles().clear();
+		for (RoleDTO roleDto : dto.getRoles()) {
+			Role role = roleRepository.getReferenceById(roleDto.getId());
+			entity.getRoles().add(role);
+		} 
+	}
+	
+}
